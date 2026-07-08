909bb098553d027d7b3182ad0b320ee6149b7cf3
jayamadhavan-v <jayamadhavan.vjm@gmail.com>
2026-07-08 22:54:02 +0530

    final commit

--- src/components/map/WeatherLegend.jsx
+++ src/components/map/WeatherLegend.jsx
@@ -7,7 +7,7 @@ const WeatherLegend = () => {
 
    const selectedLayer = useWeatherStore((state) => state.selectedLayer);
    const legend = LEGEND_CONFIG[selectedLayer] || LEGEND_CONFIG.temp_new;
-
+// i love coding 
    return (
       <div className="absolute bottom-28 right-4 z-[1000] bg-white rounded-lg shadow-lg px-4 py-2 sm:bottom-6" >
          <h3 className='font-semibold text-gray-800 mb-3'>
